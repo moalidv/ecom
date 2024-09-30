365995446d3d05a3380d1d93be74d178c00ed9fb
Mohamed Ali Alkassas <elasasmohamed97@gmail.com>
2024-10-01 00:22:56 +0300

    Create root layout file
    set general layout components like header, navbar and footer.

---
+++ src/components/common/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+const { navbar } = styles;
+
+export const Navbar = () => {
+  return <div className={navbar}>Navbar</div>;
+};
+
+export default Navbar;
